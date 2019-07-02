f4c8bad00d944e1de11b283114dbec9673bd0d74
Rovert4824 <trevormaster@comcast.net>
2019-07-02 09:33:33 -0600

    first commit

---
+++ src/components/app.js
@@ -0,0 +1,19 @@
+// Imports the `React` module and the `Component` class from that module.
+import React, { Component } from 'react';
+
+// Creates an `App` class that is a child of the `Component` class.
+class App extends Component {
+
+  render() {
+    
+    return (
+
+      <div className='app'>
+        Working
+      </div>
+
+    );
+  }
+}
+
+export default App;
